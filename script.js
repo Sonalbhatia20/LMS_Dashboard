a7ae5c6034d12467326b0a05222b5dcacb8d13a5
Sonalbhatia20 <sonalbhatia2087@gmail.com>
2021-05-24 11:28:08 +0530

    Second Commit

--- script.js
+++ script.js
@@ -58,7 +58,124 @@ var options = {
   var chart = new ApexCharts(document.querySelector("#apex1"), options);
   chart.render();
   
+  var options1 = {
+    chart: {
+      height: 280,
+      type: "radialBar",
+    },
+    series: [68, 82, 60, 40, 35, 75],
+    plotOptions: {
+      radialBar: {
+        dataLabels: {
+          total: {
+            show: true,
+            label: 'TOTAL'
+          }
+        }
+      }
+    },
+    labels: ['BIOLOGY', 'HISTORY', 'MATHS', 'ENGLISH', 'GEOGRAPHY', 'PHYSICS']
+  };
   
+  new ApexCharts(document.querySelector("#chart-circle1"), options1).render();
+  
+   var options2 = {
+          series: [{
+            name: " Aggregate Percentage Marks",
+            data: [58, 47, 75, 62, 87, 68, 74, 85, 91],
+        }],
+          chart: {
+          height: 300,
+          type: 'line',
+          zoom: {
+            enabled: false
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'straight'
+        },
+        grid: {
+          row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+            opacity: 0.5
+          },
+        },
+        xaxis: {
+          title: {
+            text: 'Month'
+          },
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+        },
+        yaxis: {
+          title: {
+            text: 'Percentage'
+          },
+        }
+        };
+
+  var chart2 = new ApexCharts(document.querySelector("#apex2"), options2);
+  chart2.render();
+
+  var options3 = {
+    chart: {
+      height: 280,
+      type: "radialBar",
+    },
+  
+    series: [65],
+    colors: ["#af78c0"],
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          margin: 0,
+          size: "70%",
+          background: "#514b53"
+        },
+        track: {
+          dropShadow: {
+            enabled: true,
+            top: 2,
+            left: 0,
+            blur: 4,
+            opacity: 0.15
+          }
+        },
+        dataLabels: {
+          name: {
+            offsetY: -10,
+            color: "#fff",
+            fontSize: "13px"
+          },
+          value: {
+            color: "#fff",
+            fontSize: "30px",
+            show: true
+          }
+        }
+      }
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        type: "vertical",
+        gradientToColors: ["#925fa7"],
+        stops: [0, 100]
+      }
+    },
+    stroke: {
+      lineCap: "round"
+    },
+    labels: ["Progress"]
+  };
+  
+  var chart3 = new ApexCharts(document.querySelector("#apex3"), options3);
+  
+  chart3.render();
+
   var sidebarOpen = false;
   var sidebar = document.getElementById("sidebar");
   var sidebarCloseIcon = document.getElementById("sidebarIcon");
